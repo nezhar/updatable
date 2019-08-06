8b0fa62d91f99e71b6d99ef3b5f25b7fbf09bbb0
Harald Nezbeda <hn@nezhar.com>
2019-08-06 09:02:31 +0200

    Add code coverage

---
+++ run_tests.py
@@ -0,0 +1,12 @@
+import unittest
+import doctest
+
+
+def test_suite():
+    suite = unittest.TestLoader().discover("test")
+    suite.addTest(doctest.DocFileSuite("README.rst"))
+    return suite
+
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(test_suite())
